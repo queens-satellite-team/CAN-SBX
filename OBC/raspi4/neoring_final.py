8e4195123286b16efbb1d6bd9df7677a8fc7907f
josh <joshuafwade@gmail.com>
2021-03-31 18:04:45 -0400

    add neopixel code

---
+++ OBC/raspi4/neoring_final.py
@@ -0,0 +1,85 @@
+
+import RPI.GPIO as GPIO
+import time
+import board
+import neopixel
+ 
+ 
+GPIO.setmode(GPIO.BCM)
+
+pixel_pin = board.D18
+ 
+num_pixels = 12
+ 
+ORDER = neopixel.GRB
+ 
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.004, auto_write=False, pixel_order=ORDER
+)
+ 
+ 
+pixels[0]=(0,255,0)
+pixels.show
+ 
+
+#power
+if GPIO.input(5):
+    pixels[2]=(255,0,0)
+    pixels.show
+    time.sleep(1)
+else:
+    pixels[2]=(0,0,0)
+    pixels.show
+    time.sleep(1)
+
+if GPIO.input(6):
+    pixels[3]=(255,0,0)
+    pixels.show
+    time.sleep(1)
+else:
+    pixels[3]=(0,0,0)
+    pixels.show
+    time.sleep(1)
+    
+#payload
+if GPIO.input(13):
+    pixels[5]=(255,255,0)
+    pixels.show
+    time.sleep(1)
+else:
+    pixels[5]=(0,0,0)
+    pixels.show
+    time.sleep(1)
+    
+if GPIO.input(19):
+    pixels[6]=(255,255,0)
+    pixels.show
+    time.sleep(1)
+else:
+    pixels[6]=(0,0,0)
+    pixels.show
+    time.sleep(1)
+    
+#comms
+if GPIO.input(26):
+    pixels[8]=(0,0,255)
+    pixels.show
+    time.sleep(1)
+else:
+    pixels[8]=(0,0,0)
+    pixels.show
+    time.sleep(1)
+    
+#ADCS
+if GPIO.input(16):
+    pixels[10]=(255,0,255)
+    pixels.show
+    time.sleep(1)
+else:
+    pixels[10]=(0,0,0)
+    pixels.show
+    time.sleep(1)
+
+
+ 
+    
