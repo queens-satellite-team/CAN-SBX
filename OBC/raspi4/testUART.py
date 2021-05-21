758340d673c038a41bda197d86d55cecfb1af772
Calum <cal.graham707"gmail.com>
2021-05-21 22:04:23 +0100

    Add import statement and changed baudrate to match that of the ADCS system

--- OBC/raspi4/testUART.py
+++ OBC/raspi4/testUART.py
@@ -1,8 +1,9 @@
 import serial,time
+from serial import Serial
 if __name__ == '__main__':
     
     print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+    with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as arduino:
         time.sleep(0.1) #wait for serial to open
         if arduino.isOpen():
             print("{} connected!".format(arduino.port))
