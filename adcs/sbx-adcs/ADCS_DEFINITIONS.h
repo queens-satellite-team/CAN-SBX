702c03d1063a7354e9471fc14dbad9a8c68ef91c
Aidan-Sheedy <Aidan.P.Sheedy@gmail.com>
2021-06-01 22:43:07 -0400

    Each sensor can now have different sampling periods. Moved relevant definitions to seperate header.

---
+++ adcs/sbx-adcs/ADCS_DEFINITIONS.h
@@ -0,0 +1,24 @@
+/*
+   This file contains definitions relevant to the ADCS system
+   of the QSET CAN-SBX Balloon project.
+*/
+
+//Booleans controlling what is being read
+#define GPS_get_time
+#define GPS_get_coords
+#define GPS_get_alt
+#define BNO_get_orient
+#define BNO_get_gyro
+#define BNO_get_acc
+#define BNO_get_mag
+#define BNO_get_temp
+#define MPU_get_gyro
+#define MPU_get_acc
+#define MPU_get_temp
+
+//controlling read rate
+const int gps_time = 1000;
+const int bno_time = 1000;
+const int mpu_time = 1000;
+const int min_time = min(min(gps_time, bno_time), mpu_time);
+const int max_time = max(gps_time,max(bno_time,mpu_time));
