f8ac56c0e933b48ee644eb66e83980960b0870aa
Emma <17ep14@queensu.ca>
2021-06-06 12:41:51 -0700

    Reverse the logic of the led program so that the leds turn on when there is a problem with the subsystem.

--- OBC/raspi4/neoring_final-jake.py
+++ OBC/raspi4/neoring_final-jake.py
@@ -4,20 +4,29 @@ import time
 import board
 import neopixel
 
-#GPIO pins for each subsystem on the pi
-comms = 26
-eps = 5
-adcs = 16
-payload1 = 13
-payload2 = 19
+#GPIO pins that each subsystem connects to on the pi
+payload1Pin = 13
+payload2Pin = 19
+adcsPin = 16
+commsPin = 26
+epsPin = 5
+
+#led numbers for each subsystem
+statusLed = 0
+payload1Led = 2
+payload2Led = 4
+adcsLed = 6
+commsLed = 8
+epsLed = 10
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(eps, GPIO.IN)
-GPIO.setup(payload1, GPIO.IN)
-GPIO.setup(payload2, GPIO.IN)
-GPIO.setup(comms, GPIO.IN)
-GPIO.setup(adcs, GPIO.IN)
-GPIO.setup(18, GPIO.OUT) #emma - what is this one for?
+GPIO.setup(statusLed, GPIO.IN)
+GPIO.setup(payload1Pin, GPIO.IN)
+GPIO.setup(payload2Pin, GPIO.IN)
+GPIO.setup(adcsPin, GPIO.IN)
+GPIO.setup(commsPin, GPIO.IN)
+GPIO.setup(epsPin, GPIO.IN)
+GPIO.setup(18, GPIO.OUT) 
 pixel_pin = board.D18
  
 num_pixels = 12
@@ -33,68 +42,49 @@ for i in range(12):
     pixels[i] = (0,0,0)
     pixels.show()
  
-pixels[0]=(0,255,0)
-pixels.show()
- 
 while 1:
-#power
-    if GPIO.input(5):
-        pixels[2]=(255,0,0)
+    if GPIO.input(payload1Pin) and GPIO.input(payload2Pin) and GPIO.input(epsPin) and GPIO.input(adcsPin) and GPIO.input(commsPin):
+        pixels[statusLed] = (0, 255, 0)  #this should be green
         pixels.show()
-        time.sleep(1)
-    else:
-        pixels[2]=(0,0,0)
-        pixels.show()
-        time.sleep(1)
+        time.sleep(5)
+    else :
+        pixels[statusLed] = (255, 0, 0) #this should be red
 
-    if GPIO.input(6):
-        pixels[3]=(255,0,0)
-        pixels.show()
-        time.sleep(1)
-    else:
-        pixels[3]=(0,0,0)
-        pixels.show()
-        time.sleep(1)
-    
 #payload
-    if GPIO.input(13):
-        pixels[5]=(255,255,0)
+    if GPIO.input(payload1Pin):
+        pixels[payload1Led]=(0,0,0)
         pixels.show()
-        time.sleep(1)
     else:
-        pixels[5]=(0,0,0)
+        pixels[payload1Led]=(255,255,0)
         pixels.show()
-        time.sleep(1)
-    
-    if GPIO.input(19):
-        pixels[6]=(255,255,0)
+
+    if GPIO.input(payload2Pin):
+        pixels[payload2Led]=(0,0,0)
         pixels.show()
-        time.sleep(1)
     else:
-        pixels[6]=(0,0,0)
+        pixels[payload2Led]=(255,255,0)
         pixels.show()
-        time.sleep(1)
     
+#adcs
+    if GPIO.input(adcsPin):
+        pixels[adcsLed]=(0,0,0)
+        pixels.show()
+    else:
+        pixels[adcsLed]=(255,0,255)
+        pixels.show()
 #comms
-    if GPIO.input(26):
-        pixels[8]=(0,0,255)
-        pixels.show()
-        time.sleep(1)
-    else:
-        pixels[8]=(0,0,0)
+    if GPIO.input(commsPin):
+        pixels[commsLed]=(0,0,0)
         pixels.show
-        time.sleep(1)
-    
-#ADCS
-    if GPIO.input(16):
-        pixels[10]=(255,0,255)
-        pixels.show()
-        time.sleep(1)
     else:
-        pixels[10]=(0,0,0)
+        pixels[commsLed]=(0,0,255)
         pixels.show()
-        time.sleep(1)
-
-
- 
-    
+        
+#eps
+    if GPIO.input(epsPin):
+        pixels[epsLed]=(0,0,0)
+        pixels.show()
+    else:
+        pixels[epsLed]=(255,0,0)
+        pixels.show()
+       
