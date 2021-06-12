8fa662c0558a82087b3c9533f7bc65c641ce1eeb
josh <16jrm11@queensu.ca>
2021-06-12 16:08:23 -0400

    Update code to transmit ADCS orientation data to i2c bus and receive by STM32 board

--- OBC/raspi4/rpi_i2c/arduino-stm.py
+++ OBC/raspi4/rpi_i2c/arduino-stm.py
@@ -1,10 +1,12 @@
+
+
 #RPi Pinouts
 
 #I2C Pins 
 #GPIO2 -> SDA
 #GPIO3 -> SCL
 
-#Import the Library Requreid 
+#mport the Library Required 
 import smbus
 import serial,time
 from serial import Serial
@@ -26,29 +28,32 @@ def readNumber():
     number = bus.read_byte_data(address, 1)
     return number
 
+
 if __name__ == '__main__':
+    i=1
     print('Running. Press CTRL-C to exit.')
     with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as arduino:
-        time.sleep(0.1) #wait for serial to open
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-
-              
-                
+        time.sleep(0.5) #wait for serial to open
+        answer=arduino.readline().decode('utf-8').rstrip()
+        arduino.flush()
+        while answer != 'MPU Initialized':
+            if arduino.isOpen():
+                if  arduino.in_waiting > 0:
+                    answer=arduino.readline().decode('utf-8').rstrip()
+                    print(answer)
+        while 'true':
+            if arduino.isOpen():
+                if  arduino.in_waiting > 0:
+                    answer=arduino.readline().decode('utf-8').rstrip()
+                    print(answer)
+                    data_list=list(answer)
+                    writeNumber(ord('<'))
+                    for i in data_list:
+                        #Sends to the Slaves 
+                        writeNumber(int(ord(i)))
+                        time.sleep(.01)
+                    writeNumber(ord('>'))
+                time.sleep(0.2)    
         else:
             print("no data!")
     
-buffer = arduino.inWaiting()
-print(buffer)
-while True:
-    #Receives the data from the User
-    if  arduino.inWaiting()>0: 
-                 answer=arduino.readline(8)
-                 print(answer)
-    data_list = list(answer)
-    for i in data_list:
-        #Sends to the Slaves 
-        writeNumber(int(ord(i)))
-        time.sleep(.01)
-    writeNumber(int(0x0A))
-    arduino.flushInput() #remove data after reading
