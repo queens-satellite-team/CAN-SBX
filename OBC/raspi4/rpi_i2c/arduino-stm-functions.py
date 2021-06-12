baca619610ba48f781322a82a6cc0205034dc225
josh <16jrm11@queensu.ca>
2021-06-12 16:57:18 -0400

    Change arduino-stm code to be built into functions (not working yet)

---
+++ OBC/raspi4/rpi_i2c/arduino-stm-functions.py
@@ -0,0 +1,67 @@
+
+
+#RPi Pinouts
+
+#I2C Pins 
+#GPIO2 -> SDA
+#GPIO3 -> SCL
+
+#mport the Library Required 
+import smbus
+import serial,time
+from serial import Serial
+
+
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
+
+#Slave Address
+address = 0x04
+
+
+def writeNumber(value):
+    bus.write_byte(address, value)
+    return -1
+
+
+def readNumber():
+    number = bus.read_byte_data(address, 1)
+    return number
+
+def OrientationInit():
+       if __name__ == '__main__':
+            print('Running. Press CTRL-C to exit.')
+            with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as arduino:
+                time.sleep(0.5) #wait for serial to open
+                answer=arduino.readline().decode('utf-8').rstrip()
+                arduino.flush()
+                while answer != 'MPU Initialized':
+                    if arduino.isOpen():
+                        if  arduino.in_waiting > 0:
+                            answer=arduino.readline().decode('utf-8').rstrip()
+                            print(answer)
+                            
+                           
+
+def OrientationTransmit():
+                if arduino.isOpen():
+                    if  arduino.in_waiting > 0:
+                        answer=arduino.readline().decode('utf-8').rstrip()
+                        print(answer)
+                        data_list=list(answer)
+                        writeNumber(ord('<'))
+                        for i in data_list:
+                            #Sends to the Slaves 
+                            writeNumber(int(ord(i)))
+                            time.sleep(.01)
+                        writeNumber(ord('>'))
+                    time.sleep(0.2)
+                else:
+                    print("no data!")
+        
+OrientationInit()
+if __name__ == '__main__':
+    with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as arduino:
+        time.sleep(0.5) #wait for serial to open
+        for i < 10
+        OrientationTransmit()
