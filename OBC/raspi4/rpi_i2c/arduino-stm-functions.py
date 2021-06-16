079724bb3714877a003d6bf8c27f551a8049c1ab
josh <16jrm11@queensu.ca>
2021-06-16 17:15:08 -0400

    Add code to make the functions work in an infinite while loop while still being able to do other tasks

--- OBC/raspi4/rpi_i2c/arduino-stm-functions.py
+++ OBC/raspi4/rpi_i2c/arduino-stm-functions.py
@@ -9,6 +9,7 @@
 #mport the Library Required 
 import smbus
 import serial,time
+import linecache
 from serial import Serial
 
 
@@ -17,7 +18,8 @@ bus = smbus.SMBus(1)
 
 #Slave Address
 address = 0x04
-
+global LinePos
+LinePos = 0
 
 def writeNumber(value):
     bus.write_byte(address, value)
@@ -29,39 +31,45 @@ def readNumber():
     return number
 
 def OrientationInit():
-       if __name__ == '__main__':
-            print('Running. Press CTRL-C to exit.')
-            with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as arduino:
-                time.sleep(0.5) #wait for serial to open
-                answer=arduino.readline().decode('utf-8').rstrip()
-                arduino.flush()
-                while answer != 'MPU Initialized':
-                    if arduino.isOpen():
-                        if  arduino.in_waiting > 0:
-                            answer=arduino.readline().decode('utf-8').rstrip()
-                            print(answer)
+        print('Running. Press CTRL-C to exit.')
+        answer=arduino.readline().decode('utf-8').rstrip()
+        arduino.flush()
+        while answer != 'MPU Initialized':
+            if arduino.isOpen():
+                if  arduino.in_waiting > 0:
+                    answer=arduino.readline().decode('utf-8').rstrip()
+                    #answer=arduino.readline().decode('utf-8').rstrip()
+                    print(answer)
+        arduino.flush()
+                           
                             
                            
 
 def OrientationTransmit():
-                if arduino.isOpen():
-                    if  arduino.in_waiting > 0:
-                        answer=arduino.readline().decode('utf-8').rstrip()
-                        print(answer)
-                        data_list=list(answer)
-                        writeNumber(ord('<'))
-                        for i in data_list:
-                            #Sends to the Slaves 
-                            writeNumber(int(ord(i)))
-                            time.sleep(.01)
-                        writeNumber(ord('>'))
-                    time.sleep(0.2)
-                else:
-                    print("no data!")
+                        if arduino.isOpen():
+                            if  arduino.in_waiting > 0:
+                                answer=arduino.readline().decode('utf-8').rstrip()
+                                print(answer)
+                                data_list=list(answer)
+                                writeNumber(ord('<'))
+                                for i in data_list:
+                                    #Sends to the Slaves 
+                                    writeNumber(int(ord(i)))
+                                    time.sleep(.01)
+                                writeNumber(ord('>'))
+                            time.sleep(0.1)
+                        else:
+                            print("no data!")
+                        arduino.flush()
+                        
         
-OrientationInit()
 if __name__ == '__main__':
     with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as arduino:
-        time.sleep(0.5) #wait for serial to open
-        for i < 10
-        OrientationTransmit()
+        time.sleep(1) #wait for serial to open
+        answer=arduino.readline().decode('utf-8').rstrip()
+        arduino.flush()
+        OrientationInit()
+        while ('true'):    
+            OrientationTransmit()
+print("Finished!")
+            
